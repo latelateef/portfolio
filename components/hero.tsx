92d228f955d88893ad8baae4288322302de49884
Shubham Saini <sainishubham9928@gmail.com>
2025-04-12 22:40:12 +0530

    added text-generation effect

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,6 +2,8 @@
 
 import { motion } from "framer-motion"
 import { Button } from "@/components/ui/button"
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
 
 export function Hero() {
   return (
@@ -13,13 +15,14 @@ export function Hero() {
           transition={{ duration: 0.5 }}
           className="text-center"
         >
-          <h1 className="text-5xl md:text-6xl font-bold mb-6 tracking-tight">Hey, I'm Shubham Singh Saini 👋</h1>
+          <h1 className="text-5xl md:text-6xl font-bold mb-6 tracking-tight">
+          <TextGenerateEffect words={"Hey, I'm Shubham Singh Saini 👋"} />
+            </h1>
           <h2 className="text-2xl md:text-3xl font-medium mb-8 text-gray-600 dark:text-gray-400">
-          Undergrad in 6th Sem at IIIT Allahabad | Full-Stack Developer
+          <TextGenerateEffect words={"Undergrad in 6th Sem at IIIT Allahabad | Full-Stack Developer"} />
           </h2>
           <p className="text-lg md:text-xl max-w-2xl mx-auto mb-12 text-gray-700 dark:text-gray-300">
-          I'm a full-stack developer passionate about building scalable web apps with clean design and smooth user experiences. Always learning and working, I focus on delivering solutions that are simple yet impactful.
-
+          <TextGenerateEffect words={"I'm a full-stack developer passionate about building scalable web apps with clean design and smooth user experiences. Always learning and working, I focus on delivering solutions that are simple yet impactful."} />
           </p>
           <div className="flex justify-center gap-4">
             <Button asChild className="px-8 py-6 text-base">
